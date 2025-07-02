69ab2ea07062a2c3170be2320e331ca80a2be733
vishalsingh1175 <vishalchhonker007@gmail.com>
2025-07-02 08:41:30 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static boolean[] map = new boolean[26];
+    public static String newstr ="";
+
+    public static void main(String[] args) throws Exception {
+        // recursion practice
+        String str = "abbccdda";
+        delDup(str,0);
+
     }
+    public static void delDup(String str,int idx){
+
+        //base case
+       if(idx == str.length()){
+           System.out.println(newstr);
+           return;
+       }
+       if(map[str.charAt(idx) - 'a'] == false){
+           newstr +=str.charAt(idx);
+           map[str.charAt(idx) - 'a'] = true;
+       }
+       delDup(str,idx+1);
+
+    }
+
 }
